22cd1dccb229960502728320b2571408e5688f90
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-08-04 16:16:36 +0200

    Added header component with the logo.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../assets/Logo.png'
+function Header() {
+    return (
+        <>
+            <img src={logo} alt=""/>
+        </>
+    )
+}
+
+export default Header
